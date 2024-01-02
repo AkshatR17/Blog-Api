02cb124c86e2ef843cf1eb75aad56a8239945037
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-02 16:14:24 +0530

    Replaced in memory data structure with database

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
+import { data } from "./secrets.js";
 
 const app = express();
 const port = 4000;
 
-// In-memory data store
-let posts = [
-  {
-    id: 1,
-    title: "The Rise of Decentralized Finance",
-    content:
-      "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
-    author: "Alex Thompson",
-    date: "2023-08-01T10:00:00Z",
-  },
-  {
-    id: 2,
-    title: "The Impact of Artificial Intelligence on Modern Businesses",
-    content:
-      "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
-    author: "Mia Williams",
-    date: "2023-08-05T14:30:00Z",
-  },
-  {
-    id: 3,
-    title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
-    content:
-      "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
-    author: "Samuel Green",
-    date: "2023-08-10T09:15:00Z",
-  },
-];
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: data.database,
+  password: data.password,
+  port: data.port
+});
+
+db.connect();
 
 let lastId = 3;
 
@@ -41,19 +25,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
-app.get('/posts', (req, res) => {
+app.get('/posts', async (req, res) => {
+  const result = await db.query(`SELECT * FROM posts`);
+  const posts = result.rows;
   res.json(posts);
 });
 
 //CHALLENGE 2: GET a specific post by id
-app.get('/posts/:id', (req, res) => {
+app.get('/posts/:id',async (req, res) => {
 
   const id = parseInt(req.params.id);
-  const post = posts.find((post) => {
-    if (post.id === id) {
-      return post;
-    }
-  });
+  const post = await db.query(`SELECT * FROM posts WHERE id = ${id}`);
   if (post) {
     res.json(post);
   }
@@ -111,17 +93,17 @@ app.patch('/posts/:id', (req, res) => {
 });
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
-app.delete('/posts/:id', (req,res)=>{
+app.delete('/posts/:id', (req, res) => {
 
   const id = parseInt(req.params.id);
   const postIndex = posts.findIndex((post) => post.id === id);
-  
+
   if (postIndex > -1) {
     const post = posts[postIndex];
     posts.splice(postIndex, 1);
     res.json(post);
   }
-  else{
+  else {
     res.status(404);
     res.json({ message: "Post not found" });
   }
